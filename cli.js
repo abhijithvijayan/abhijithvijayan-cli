309a0b787ab3955932a3961f52137b51f6b8f299
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-07-04 14:08:58 +0530

    fix: apply linting

--- cli.js
+++ cli.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-const meow = require('meow');
 const showBanner = require('node-banner');
 const importJsx = require('import-jsx');
+const {render} = require('ink');
 const React = require('react');
-const { render } = require('ink');
+const meow = require('meow');
 
 const ui = importJsx('./ui');
 
@@ -14,7 +14,11 @@ meow(`
 `);
 
 (async () => {
-	await showBanner('Abhijith Vijayan', "I build things that aren't evil.", 'blue');
+  await showBanner(
+    'Abhijith Vijayan',
+    "I build things that aren't evil.",
+    'blue'
+  );
 
-	render(React.createElement(ui));
+  render(React.createElement(ui));
 })();
--- ui.js
+++ ui.js
@@ -1,69 +1,71 @@
-const open = require('open');
-const { Box } = require('ink');
-const React = require('react');
 const SelectInput = require('ink-select-input').default;
+const React = require('react');
+const open = require('open');
+const {Box} = require('ink');
 
-const handleSelect = item => {
-	if (item.url) {
-		open(item.url);
-	} else if (item.action) {
-		item.action();
-	}
+const handleSelect = (item) => {
+  if (item.url) {
+    open(item.url);
+  } else if (item.action) {
+    item.action();
+  }
 };
 
-const createList = items => {
-	return items.map(item => {
-		return { ...item, key: item.label || item.url };
-	});
+const createList = (items) => {
+  return items.map((item) => {
+    return {...item, key: item.label || item.url};
+  });
 };
 
 const listItems = createList([
-	{
-		label: 'Website 🏠',
-		url: 'https://abhijithvijayan.in',
-	},
-	{
-		label: 'Code 💻',
-		url: 'https://github.com/abhijithvijayan',
-	},
-	{
-		label: 'Tweets ⚡',
-		url: 'https://twitter.com/_abhijithv',
-	},
-	{
-		label: 'Writings 📝',
-		url: 'https://medium.com/@abhijithvijayan',
-	},
-	{
-		label: 'Hire 💼',
-		url: 'https://linkedin.com/in/iamabhijithvijayan',
-	},
-	{
-		label: 'Contact 📩',
-		url: 'mailto:email@abhijithvijayan.in',
-	},
-	{
-		label: 'Support my open source work 💌',
-		url: 'https://abhijithvijayan.in/donate',
-	},
-	{
-		label: '---------',
-	},
-	{
-		label: 'Quit',
-		action() {
-			process.exit();
-		},
-	},
+  {
+    label: 'Website 🏠',
+    url: 'https://abhijithvijayan.in',
+  },
+  {
+    label: 'Code 💻',
+    url: 'https://github.com/abhijithvijayan',
+  },
+  {
+    label: 'Tweets ⚡',
+    url: 'https://twitter.com/_abhijithv',
+  },
+  {
+    label: 'Writings 📝',
+    url: 'https://medium.com/@abhijithvijayan',
+  },
+  {
+    label: 'Hire 💼',
+    url: 'https://linkedin.com/in/iamabhijithvijayan',
+  },
+  {
+    label: 'Contact 📩',
+    url: 'mailto:email@abhijithvijayan.in',
+  },
+  {
+    label: 'Support my open source work 💌',
+    url: 'https://abhijithvijayan.in/donate',
+  },
+  {
+    label: '---------',
+  },
+  {
+    label: 'Quit',
+    action() {
+      process.exit();
+    },
+  },
 ]);
 
 const renderBox = () => {
-	return (
-		<Box flexDirection="column">
-			<Box marginBottom={1} />
-			<SelectInput items={listItems} onSelect={handleSelect} />
-		</Box>
-	);
+  return (
+    <>
+      <Box flexDirection="column">
+        <Box marginBottom={1} />
+        <SelectInput items={listItems} onSelect={handleSelect} />
+      </Box>
+    </>
+  );
 };
 
 module.exports = renderBox;
